0046a454adf9fb4997aff0341570e6a45eb5156f
Andreea Trocan <andreeatrocan@gmail.com>
2018-09-20 15:03:27 +0300

    Add Expensify styles

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -26,22 +26,30 @@ export class ExpenseListFilters extends React.Component {
         };
     render() {
         return (
-            <div>
+            <div className="content-container">
+            <div className="input-group">
+                <div className="input-group__item">
                 <input 
-                    type="text" 
-                    value={this.props.filters.text} 
-                    onChange={this.onTextChange} 
+                type="text" 
+                className="text-input"
+                placeholder="Search expenses"
+                value={this.props.filters.text} 
+                onChange={this.onTextChange} 
                 />
-                
-                <select 
+                </div>
+                <div className="input-group__item">
+                    <select 
+                    className="select"
                     value={this.props.filters.sortBy} 
                     onChange={this.onSortChange}
                 
-                >
+                    >
                     <option value="date">Date</option>
                     <option value="amount">Amount</option>
-                </select>
-                <DateRangePicker
+                    </select>
+                </div>
+                <div className="input-group__item">
+                    <DateRangePicker
                     startDate={this.props.filters.startDate}
                     endDate={this.props.filters.endDate}
                     onDatesChange={this.onDatesChange}
@@ -50,7 +58,9 @@ export class ExpenseListFilters extends React.Component {
                     showClearDates={true}
                     numberOfMonths={1}
                     isOutsideRange={() => false}
-                />
+                    />
+                </div>
+            </div>
             </div>
         );
     }
